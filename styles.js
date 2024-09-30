e7e2ff9b71a5fdb1a35692e2779e7188de3cc6ed
Kennedy <ustrodakenzy@gmail.com>
2024-09-30 00:13:33 +0000

    final project

---
+++ styles.js
@@ -0,0 +1,7 @@
+const toggleButton = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
